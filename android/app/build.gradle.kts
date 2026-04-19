569913c75a90679db75cabef3fc6454e0bd9ebbe
Paras Arora <parasarora1224@gmail.com>
2026-04-19 18:10:40 +0530

    support all platform

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.movie_app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.movie_app"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+//  flavorDimensions += "default" this is powerful for creating
+    flavorDimensions += "default"
+
+    productFlavors {
+        create("development") {
+            applicationId "com.paras.development" // This is the base applicationId for the development flavor
+            dimension = "default"
+            manifestPlaceholders = [appName: "development"]
+//            applicationIdSuffix = ".dev"  // This will append .dev to the applicationId, making it com.paras.development.dev
+        }
+
+        create("staging") {
+            applicationId "com.paras.staging" //
+            dimension = "default"
+            manifestPlaceholders = [appName: 'staging']
+//            applicationIdSuffix = ".stg"
+        }
+
+        create("production") {
+            applicationId "com.paras.movieapp" //
+            dimension = "default"
+            manifestPlaceholders = [appName: "production"]
+//            applicationIdSuffix = ""
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
--- notes/flavor_setup.md
+++ notes/flavor_setup.md
@@ -1,2 +1,40 @@
+Flutter flavor setup
+/// Android Folder
+Step 1:  android/app/build.gradle.kts
+Expand the app folder and open the build.gradle file. Inside the android block, after buildTypes,
+add the following:
+![img_1.png](img_1.png)
+flavorDimensions += "default"
+
+    productFlavors {
+        development {
+            applicationId "com.paras.development" // This is the base applicationId for the development flavor
+            dimension = "default"
+            manifestPlaceholders = [appName: "development"]
+
+// applicationIdSuffix = ".dev"  // This will append .dev to the applicationId, making it
+com.paras.development.dev
+}
+
+        staging {
+            applicationId "com.paras.staging" //
+            dimension = "default"
+            manifestPlaceholders = [appName: 'staging']
+
+// applicationIdSuffix = ".stg"
+}
+
+        production {
+            applicationId "com.paras.movieapp" //
+            dimension = "default"
+            manifestPlaceholders = [appName: "production"]
+
+// applicationIdSuffix = ""
+}
+}
+
+Step 2: Modify the AndroidManifest.xml file
+Replace the label property with "${appName}".
 ![img.png](img.png)
 
+//// IOS 
